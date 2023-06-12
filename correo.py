af7c4cfc8cda7773c69a849ca8364caf53742834
albercosta22 <133118310+albercosta22@users.noreply.github.com>
2023-06-12 17:25:18 +0200

    correo.py

--- correo.py
+++ correo.py
@@ -1,29 +1,41 @@
-import json
-import smtplib
 import boto3
+import json
+
+def send_email(subject, body):
+    ses = boto3.client('ses', region_name='us-east-1')  
+    sender_email = 'albercm22@hotmail.com'  
+    recipient_email = 'albercm22@hotmail.com'  
+    
+    email_body = f"""
+    <html>
+    <head></head>
+    <body>
+      <h1>{subject}</h1>
+      <p>{body}</p>
+    </body>
+    </html>
+    """
+    
+    response = ses.send_email(
+        Source=sender_email,
+        Destination={'ToAddresses': [recipient_email]},
+        Message={
+            'Subject': {'Data': subject},
+            'Body': {'Html': {'Data': email_body}}
+        }
+    )
+    
+    return response
 
 def lambda_handler(event, context):
+    if event['detail-type'] == 'EC2 Instance Launch Successful':
+        instance_id = event['detail']['EC2InstanceId']
+        subject = 'Nueva instancia creada en el grupo de autoescalado'
+        body = f'Se ha creado la instancia con ID: {instance_id}'
+        response = send_email(subject, body)
+        print(response)
     
-    remitente = "albercm22@hotmail.com" 
-    destinatario = "albcosta22@gmail.com" 
-    asunto = "Instancia creada" 
-    mensaje = """Se ha creado una nueva instancia en su servidor HTTP"""
-
-    email = """From: %s
-    To: %s 
-    MIME-Version: 1.0 
-    Content-type: text/html 
-    Subject: %s 
-    
-    %s
-    """ % (remitente, destinatario, asunto, mensaje) 
-    try: 
-        smtp = smtplib.SMTP('localhost') 
-        smtp.sendmail(remitente, destinatario, email) 
-        print("Correo enviado") 
-    except: 
-        print("""Error: el mensaje no pudo enviarse. 
-        Compruebe que sendmail se encuentra instalado en su sistema""")
     return {
-            'statusCode': 200,
+        'statusCode': 200,
+        'body': json.dumps('Correo enviado exitosamente')
     }
